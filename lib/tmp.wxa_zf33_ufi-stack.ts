d1ff21b6db47900afdef2a9d2a74f0e713e81226
Michael Milton <michael.r.milton@gmail.com>
2022-03-15 15:34:00 +1100

    Initial

--- lib/tmp.wxa_zf33_ufi-stack.ts
+++ lib/tmp.wxa_zf33_ufi-stack.ts
@@ -1,16 +1,15 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from "path";
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class TmpWxaZf33UfiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fn = new lambda.DockerImageFunction(this, 'customImageLambda', {
+      code: lambda.DockerImageCode.fromImageAsset(path.resolve(__dirname, '../src')),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TmpWxaZf33UfiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
