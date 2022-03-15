a720efb6982c7f36dfc1b0fa31479eac8a1b8973
Michael Milton <michael.r.milton@gmail.com>
2022-03-15 15:26:56 +1100

    Initial commit

---
+++ lib/tmp.wxa_zf33_ufi-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class TmpWxaZf33UfiStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'TmpWxaZf33UfiQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/tmp.wxa_zf33_ufi.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TmpWxaZf33Ufi from '../lib/tmp.wxa_zf33_ufi-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tmp.wxa_zf33_ufi-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TmpWxaZf33Ufi.TmpWxaZf33UfiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
